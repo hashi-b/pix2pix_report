6826e48e28302e3c745fd8f1a9f684af8ac85bd6
hashib <hashib@gavo.t.u-tokyo.ac.jp>
2018-07-19 20:52:42 +0900

    test

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # pix2pix_report
 eizou_media report
+結果が概ね正しいか確認できるよう実装はchainerのcodeが存在するfacadeを選択しました
+
+・実行方法
+facade_datasetをhttp://cmp.felk.cvut.cz/~tylecr1/facade/から落としbase直下にjpg及びpngを置く
+run_Unet.sh $1 ($1は結果の出力先フォルダ名) 　　　でUnet(論文中におけるL1のみ)による変換モデルの学習及び変換　GPU環境で2時間程度
+run_pix2pix.sh $1 ($1は結果の出力先フォルダ名) でcGAN(論文中におけるcGAN+L1)による変換モデルの学習及び変換 GPU環境で半日程度
+
+sh内でepochやbatch、gpu使用の有無などのオプションも変更可能
+
+画像データのIO周りに関しては以下のコードを参考にしました
+Copyright (c) 2016 Eiichi Matsumoto
+
--- run_pix2pix.sh
+++ run_pix2pix.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export PATH=/opt/cuda-8.0/bin:$PATH
-export LD_LIBRARY_PATH=/opt/cuda-8.0/lib64:$LD_LIBRARY_PATH
-export PYTHONPATH=/home/hashib/dnnlib:$PYTHONPATH
-export PYTHONPATH=/home/hashib/.local/lib:$PYTHONPATH
+#export PATH=/opt/cuda-8.0/bin:$PATH
+#export LD_LIBRARY_PATH=/opt/cuda-8.0/lib64:$LD_LIBRARY_PATH
+#export PYTHONPATH=/home/hashib/dnnlib:$PYTHONPATH
+#export PYTHONPATH=/home/hashib/.local/lib:$PYTHONPATH
 
 rootdir=./
 outdir=./result/$1
